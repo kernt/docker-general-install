c1c5bd854d76d63c34fc03e0c1ca3814a74b7bd2
tobkern <tobkern1980@googlemail.com>
2016-06-04 21:03:10 +0200

    Update machine.sh

--- machine.sh
+++ machine.sh
@@ -5,6 +5,18 @@
 #
 curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
 
-
 echo "$(docker-machine version)\n installed"
+#Installing bash completion scripts
+
+COMPLITION_ENV(
+ https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine-wrapper.bash
+ https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine-prompt.bash
+ https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine.bash
+ )
+
+for ((i = 0; i <= max_index; i++)); do
+  echo "Element $i: '${COMPLITION_ENV[i]}'"
+done
+ 
 exit 0
+
