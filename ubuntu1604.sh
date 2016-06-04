b51ff579d1db9fd00b7d35909dc3383dd06a582e
tobkern <tobkern1980@googlemail.com>
2016-06-04 14:14:32 +0200

    Update ubuntu1604.sh

--- ubuntu1604.sh
+++ ubuntu1604.sh
@@ -13,8 +13,4 @@ sudo read $2USER
 sudo usermod -aG docker $2USER
 
 
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-
 docker run hello-world
