18594965ee8360aae7392fb1e9b1bd6565a7812f
tobkern <tobkern1980@googlemail.com>
2016-10-17 18:21:40 +0200

    Create install.sh
    Initial Install script just usable on your firt try to install.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/Bin/bash
+#
+#
+#
+#
+# Just vor your firt install of Docker .
+
+########### Add source files ########
+#
+#####################################
+
+. ./                    # Find your Linux Distribution
+. ./repo/private.sh     # add private Repo
+. ./-security.sh        # Secure your installation
