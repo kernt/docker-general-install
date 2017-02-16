42ed2c990ee706d645c0b73f693010a846d134f8
tobkern <tobkern1980@googlemail.com>
2017-02-16 18:42:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,13 @@
 #
 #####################################
 
-. ./                    # Find your Linux Distribution
-. ./repo/private.sh     # add private Repo
-. ./-security.sh        # Secure your installation
+DISTRIBUTION="ubuntu1604"
+
+. ./${DISTRIBUTION}.sh                # For example Ubuntu 16.04 Distribution
+. ./dns-server.sh                     # add dns Servers
+. ./docker-compose.sh                 # Installs docker compose 
+. ./adjust-memory-swap-accounting.sh  # adjust memory swap
+. ./enable-ufw-forwarding.sh
+. ./repo/private.sh                   # add private Repo
+. ./certs.d/make-cert.sh              # Secure your server installation 
+. ./certs.d/make-client-cert.sh       # add cert for you Client
